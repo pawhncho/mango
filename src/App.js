6008e403cda8df4e31d1302a40f66f88d249a2d0
pawhncho <pawhncho@gmail.com>
2024-12-05 08:12:14 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main className="App-header">
+      <h1>My App</h1>
+      <img className="App-logo" src={Logo} alt="App logo" />
+    </main>
+  );
+};
+
+export default App;
